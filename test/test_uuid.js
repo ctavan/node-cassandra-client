7b34a9881b2326a6f677c81c248a4231734a5415
Christoph Tavan <tavan@nemata.com>
2011-12-18 12:21:25 +0100

    Introduce node-uuid and rewrite uuid-test for node-uuid (still fails however)

--- test/test_uuid.js
+++ test/test_uuid.js
@@ -1,12 +1,12 @@
 // some tests for the uuid-js module.
 
-var UUID = require('uuid-js');
+var UUID = require('node-uuid');
 
 exports['test_uuid_from_buffer'] = function(test, assert) {
   var buf = new Buffer('\u00ee\u00a1\u006c\u00c0\u00cf\u00bd\u0011\u00e0\u0017' +
           '\u000a\u00dd\u0026\u0075\u0027\u009e\u0008', 'binary');
-  var uuid = UUID.fromBytes(buf);
-  assert.strictEqual(uuid.toString(), 'eea16cc0-cfbd-11e0-170a-dd2675279e08');
+  var uuid = UUID.unparse(buf);
+  assert.strictEqual(uuid, 'eea16cc0-cfbd-11e0-170a-dd2675279e08');
   test.finish();
 };
 
@@ -14,14 +14,12 @@ exports['test_uuid_from_buffer'] = function(test, assert) {
 // that it only generates time uuids for the beginning or ending of a specific millisecond.  It should support 
 // generating multiple successive UUIDs for the same millisecond for highly concurrent applications.
 exports['test_uuid_backwards_in_time'] = function(test, assert) {
-  test.skip();
-  
   var ts = 1314735336316;
-  var uuidTs = UUID.fromTime(ts).toString();
+  var uuidTs = UUID.v1({ msecs: ts });
   // this forces the nano tracker in uuid to get set way ahead.
-  var uuidFuture = UUID.fromTime(ts + 5000).toString();
+  var uuidFuture = UUID.v1({ msecs: ts + 5000 });
   // we want to verify that the nanos used reflect ts and not ts+5000.
-  var uuidTsSame = UUID.fromTime(ts).toString();
+  var uuidTsSame = UUID.v1({ msecs: ts });
   assert.ok(uuidTs !== uuidFuture); // duh
   assert.ok(uuidTs !== uuidTsSame); // generated from same TS after going back in time.
   // but time lo should definitely be the same.
